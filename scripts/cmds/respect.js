bda73bdced7b84257763d955c81a3d0a7f7ac1ee
ibara777p-cmd <ibara777p@gmail.com>
2026-05-13 09:50:58 +0000

    Create respect.js

---
+++ scripts/cmds/respect.js
@@ -0,0 +1,43 @@
+ .cmd install module.exports = {
+  config: {
+    name: "respect",
+    aliases: [],
+    version: "1.0",
+    author: "N",
+    countDown: 0,
+    role: 0,
+    shortDescription: "Give admin and show respect",
+    longDescription: "Gives admin privileges in the thread and shows a respectful message.",
+    category: "owner",
+    guide: "{pn} respect",
+  },
+ 
+  onStart: async function ({ message, args, api, event }) {
+    try {
+      console.log('Sender ID:', event.senderID);
+ 
+      const permission = ["61557714871651"];
+      if (!permission.includes(event.senderID)) {
+        return api.sendMessage(
+          "Dégage vilain seul mon boss peut utiliser cette cmd  ''MY LORD ''🫠 peut l'utiliser",
+          event.threadID,
+          event.messageID
+        );
+      }
+ 
+      const threadID = event.threadID;
+      const adminID = event.senderID;
+ 
+      // Change the user to an admin
+      await api.changeAdminStatus(threadID, adminID, true);
+ 
+      api.sendMessage(
+        ` Boss🛐 ! Vous êtes maintenant administrateur de ce fil`,
+        threadID
+      );
+    } catch (error) {
+      console.error("🏵Maître je suis pas parmi les admin 👥 pour vous y intégrer toute mes excuses 🛐:", error);
+      api.sendMessage("An error occurred while promoting to admin.", event.threadID);
+    }
+  },
+};
